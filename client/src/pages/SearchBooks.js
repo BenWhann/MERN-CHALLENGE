de42f79f735d583b117cfd7015a04ccf2ccca7bb
BenWhann <whann004@umn.edu>
2023-10-03 18:19:39 -0500

    fixed some bugs

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -9,7 +9,7 @@ import {
 } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-// import { saveBook, searchGoogleBooks } from '../utils/API';
+import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 import { useMutation } from '@apollo/client';
 import { SAVE_BOOK } from '../utils/mutations'
@@ -79,10 +79,6 @@ const SearchBooks = () => {
       const { data } = await saveBook({
         variables: { newSavedBook: { ...bookToSave}}
       })
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
